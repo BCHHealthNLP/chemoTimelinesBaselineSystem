2a2e936f99941e79995578a0ae697bc787da8e12
Eli Goldner <egoldner@brandeis.edu>
2024-01-30 13:00:38 -0500

    Saving point

--- timelines/instance-generator/src/user/resources/org/apache/ctakes/timelines/timelines_py/src/timelines/timelines_python_pipeline.py
+++ timelines/instance-generator/src/user/resources/org/apache/ctakes/timelines/timelines_py/src/timelines/timelines_python_pipeline.py
@@ -1,15 +1,12 @@
 import warnings
-import logging
 
-from ctakes_pbj.component.pbj_receiver import start_receiver, PBJReceiver
-from ctakes_pbj.component.pbj_sender import PBJSender
+from ctakes_pbj.component.pbj_receiver import PBJReceiver
 from ctakes_pbj.pipeline.pbj_pipeline import PBJPipeline
 
 from .timeline_annotator import TimelineAnnotator
 
 
 warnings.filterwarnings("ignore")
-logger = logging.getLogger(__name__)
 
 
 def main():
