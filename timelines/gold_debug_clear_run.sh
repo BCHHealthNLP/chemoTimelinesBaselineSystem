dfa5f56b4b15caa7040c6f082eb38804bf3f4a0f
Eli Goldner <egoldner@brandeis.edu>
2024-03-12 17:09:28 -0400

    Script and piperfile updates

--- timelines/gold_debug_clear_run.sh
+++ timelines/gold_debug_clear_run.sh
@@ -10,8 +10,9 @@ java -cp instance-generator/target/instance-generator-5.0.0-SNAPSHOT-jar-with-de
      org.apache.ctakes.core.pipeline.PiperFileRunner \
      -p org/apache/ctakes/timelines/pipeline/Timelines \
      -a  mybroker \
-     -v /usr/local/miniconda3/envs/timelines-docker \
-     -i ../input/ \
+     -v ~/miniconda3/envs/timelines-docker \
+     -i ../input/text \
+     -d ../input/anafora/ \
      -o ../output \
      -l org/apache/ctakes/dictionary/lookup/fast/bsv/Unified_Gold_Dev.xml \
      --pipPbj yes \
--- timelines/gold_debug_just_run.sh
+++ timelines/gold_debug_just_run.sh
@@ -2,8 +2,8 @@ java -cp instance-generator/target/instance-generator-5.0.0-SNAPSHOT-jar-with-de
      org.apache.ctakes.core.pipeline.PiperFileRunner \
      -p org/apache/ctakes/timelines/pipeline/Timelines \
      -a  mybroker \
-     -v /usr/local/miniconda3/envs/timelines-docker \
-     -i ../input/ \
-     -o ../output \
+     -v ~/miniconda3/envs/timelines-docker \
+     -i ../input/text \
+     -d ../input/anafora/ \
      -l org/apache/ctakes/dictionary/lookup/fast/bsv/Unified_Gold_Dev.xml \
      --pipPbj yes \
