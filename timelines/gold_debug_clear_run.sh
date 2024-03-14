da62c80b3074d1be570f11316b07aaa9695feaef
Eli Goldner <egoldner@brandeis.edu>
2024-03-14 15:21:03 -0400

    Script and piper fixes for subtask 1

--- timelines/gold_debug_clear_run.sh
+++ timelines/gold_debug_clear_run.sh
@@ -9,9 +9,9 @@ mvn clean package;
 java -cp instance-generator/target/instance-generator-5.0.0-SNAPSHOT-jar-with-dependencies.jar \
      org.apache.ctakes.core.pipeline.PiperFileRunner \
      -p org/apache/ctakes/timelines/pipeline/Timelines \
-     -a  mybroker \
-     -v ~/miniconda3/envs/timelines-docker \
-     -i ../input/text \
+     -a  mybroker_2.19.1_JDK_8 \
+     -v /usr/local/miniconda3/envs/subtask1 \
+     -i ../input/notes/ \
      -d ../input/anafora/ \
      -o ../output \
      -l org/apache/ctakes/dictionary/lookup/fast/bsv/Unified_Gold_Dev.xml \
